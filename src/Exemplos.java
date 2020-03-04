e65d82a099511c7a3133dfdec3aab2736fd6ba4a
rodrigods93 <rodrigodds96@gmail.com>
2020-03-04 20:37:18 -0300

    Exemplos de metodo

--- src/Exemplos.java
+++ src/Exemplos.java
@@ -3,7 +3,18 @@ public class Exemplos {
 	// escrever somente main e CTRL+ESPAÇO para completar.Esse comando completa
 	// qualquer comando digitado.
 	public static void main(String[] args) {
-		int idade = 17;
+		
+		int idade=17;
+		ehMaiorIdade(19); // Para chamar o metodo(19) O valor dentro do metodo ira rodar o algoritmo					// dentro do metodo e dar uma resposta.
+		ehMaiorIdade(30);
+		ehMaiorIdade(idade);
+		
+		classificar(0);
+
+	}
+
+	public static void ehMaiorIdade(int idade) {
+
 		if (idade >= 18) {
 
 			System.out.println("Maior de Idade");// para facilitar digitar somente syso e CTRL+ESPAÇO e completa
@@ -14,6 +25,25 @@ public class Exemplos {
 			System.out.println("Menor de Idade");
 
 		}
-	}
 
+	}
+	
+	public static void classificar(int num) {
+		if (num<0) {
+			System.out.println("Negativo");
+			
+		} else if (num >0) {
+			System.out.println("Positivo");
+
+		}
+		else {
+			System.out.println("Nulo");
+		}
+	}
 }
+
+// Public: Modificador de acesso para que outras classes tenham acesso ao
+// codigo.
+
+// void:Não retorna valor
+// nome do metodo.
