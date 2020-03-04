ad73cef5b24df463fe85eb14b7079713d37e1b30
rodrigods93 <rodrigodds96@gmail.com>
2020-03-04 20:00:24 -0300

    Revisao de IF

---
+++ src/Exemplos.java
@@ -0,0 +1,19 @@
+
+public class Exemplos {
+	// escrever somente main e CTRL+ESPAÇO para completar.Esse comando completa
+	// qualquer comando digitado.
+	public static void main(String[] args) {
+		int idade = 17;
+		if (idade >= 18) {
+
+			System.out.println("Maior de Idade");// para facilitar digitar somente syso e CTRL+ESPAÇO e completa
+													// automaticamente.
+
+		} else {
+
+			System.out.println("Menor de Idade");
+
+		}
+	}
+
+}
