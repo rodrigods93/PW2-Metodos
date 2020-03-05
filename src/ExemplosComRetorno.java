a447d495431302691a6788634033b26bb8a278d0
rodrigods93 <rodrigodds96@gmail.com>
2020-03-04 21:58:37 -0300

    Inclusao de comentarios no metodo retorno

--- src/ExemplosComRetorno.java
+++ src/ExemplosComRetorno.java
@@ -1,24 +1,26 @@
 
 public class ExemplosComRetorno {
 
-	public static String concatenar(String nome, String sobrenome) { // quando declarar uma string colocar o primeiro s em
-																	// maiusculo.
+	public static String concatenar(String nome, String sobrenome) { // quando declarar uma string colocar o primeiro s
+																		// em
+																		// maiusculo.
 
 		return nome + " " + sobrenome;
 	}
 
-	public static int tamanhoNomeCompleto(String nome,String sobrenome) {
-		String nomeCompleto = concatenar (nome,sobrenome);
+	public static int tamanhoNomeCompleto(String nome, String sobrenome) {
+		String nomeCompleto = concatenar(nome, sobrenome);
 		return nomeCompleto.length(); // vai retornar a quantidade de caracteres dentro da String.
 	}
-	
+
 	public static void main(String[] args) {
-		
-		String nome = concatenar("Rodrigo","Dantas");
-		int tamanho = tamanhoNomeCompleto("Rodrigo","Dantas");
-		System.out.println("Nomes:"+ nome);
+
+		String nome = concatenar("Rodrigo", "Dantas"); // A string nome sera igual o metodo concatenar
+		int tamanho = tamanhoNomeCompleto("Rodrigo", "Dantas");// a variavel tamanho vai ser igual ao metodo
+																// "tamanhoNomeCompleto"
+		System.out.println("Nomes:" + nome);
 		System.out.println("Qtde: " + tamanho + " caracteres");
-		
+
 	}
 
 }
